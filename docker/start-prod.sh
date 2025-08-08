abc28b1a0d7e88a4bb8f86f0b39d7c918117a0df
Heyjinu <norang2810@naver.com>
2025-08-08 23:04:00 +0900

    chore:start-prod.sh 코드 수정

--- docker/start-prod.sh
+++ docker/start-prod.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 echo "🚀 [START] 배포 환경 실행"
-# 1. 배포용 환경변수 복사
-cp ./env.prod .env
 
-# 2. 빌드 및 실행
+# 1. 스크립트가 있는 디렉토리 기준으로 이동
+cd "$(dirname "$0")"
+
+# 2. 배포용 환경변수 복사
+cp ../env.prod .env
+
+# 3. 컨테이너 정리 후 재시작
 docker-compose down -v
 docker-compose up -d --build
