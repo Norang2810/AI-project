8e7d5b20d49ebbe04ed8bc5c6d8b52a9b5bdb7bf
Heyjinu <norang2810@naver.com>
2025-08-08 23:24:48 +0900

    chore:start-prod.sh 파일 .env 파일 기준 경로 수정

--- docker/start-prod.sh
+++ docker/start-prod.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 echo "🚀 [START] 배포 환경 실행"
 
-# 1. 스크립트가 있는 디렉토리 기준으로 이동
-cp $(dirname "$0")/env.prod $(dirname "$0")/.env
+# 1. 배포용 환경변수 복사 (경로 안전하게)
+cp "$(dirname "$0")/env.prod" "$(dirname "$0")/.env"
 
-
-# 2. 배포용 환경변수 복사
-cp ../env.prod .env
-
-# 3. 컨테이너 정리 후 재시작
+# 2. 빌드 및 실행
 docker-compose down -v
 docker-compose up -d --build
